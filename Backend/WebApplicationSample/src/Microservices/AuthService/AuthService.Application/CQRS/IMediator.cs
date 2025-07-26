82a29d331579ea7470986a0f996abd0bf69dc3d3
HPOD00019 <yctaba352@gmail.com>
2025-07-26 20:56:12 +0700

    Add CQRS with my own mediator and sync handlers

---
+++ Backend/WebApplicationSample/src/Microservices/AuthService/AuthService.Application/CQRS/IMediator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthService.Application.CQRS
+{
+    interface IMediator
+    {
+        T SendQuery<T>(IRequestQuery<T> request);
+        void SendCommand (IRequestCommand command);
+    }
+
+}
---
+++ Backend/WebApplicationSample/src/Microservices/AuthService/AuthService.Application/CQRS/Mediator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthService.Application.CQRS
+{
+    class Mediator : IMediator
+    {
+        private readonly IServiceProvider _requestHandlers;
+        public Mediator(IServiceProvider requestServices)
+        {
+            _requestHandlers = requestServices;
+        }
+
+        public TResponse SendQuery<TResponse>(IRequestQuery<TResponse> request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendCommand(IRequestCommand command)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
