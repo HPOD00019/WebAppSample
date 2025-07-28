dd78145ad92ff58e388df16195ba843872fc8ecc
HPOD00019 <yctaba352@gmail.com>
2025-07-28 20:37:31 +0700

    Added CQRS with MediatR lib

--- Backend/WebApplicationSample/src/Microservices/AuthService/AuthService.Application/Commands/Register/RegisterUserCommand.cs
+++ Backend/WebApplicationSample/src/Microservices/AuthService/AuthService.Application/Commands/Register/RegisterUserCommand.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AuthService.Application.Commands.Register
 {
+    public record RegisterUserCommand : IRequest<Guid>
+    {
+        [Required] public string UserName { get; init; }
+        [Required] public string Password { get; init; }
 
+        [Required]
+        [EmailAddress]
+        public string Email { get; init; }
+    }
 }
